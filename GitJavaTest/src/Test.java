fbd18770cacddabbf965904d4bd17812d4f6dfee
Human <human1993@hotmail.de>
2015-09-19 18:17:15 +0200

    2

--- GitJavaTest/src/Test.java
+++ GitJavaTest/src/Test.java
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-
-
-public class Test {
-
- public static void main(String[] args) {
-	System.out.println("Hallo");
-}
-}
-=======
 
 public class Test {
 
@@ -15,7 +5,6 @@ public class Test {
 	System.out.println("Hallo");
 	
 	
-	// 1
+	// 2
 }
 }
->>>>>>> origin/master
